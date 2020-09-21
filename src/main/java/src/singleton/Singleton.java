630ad2dad2533e463617d78cd4790acb1de44c50
Roberto Kendy Sawamura <rkendy@gmail.com>
2020-09-21 12:59:05 -0300

    Add comment to singleton

--- src/main/java/src/singleton/Singleton.java
+++ src/main/java/src/singleton/Singleton.java
@@ -4,12 +4,16 @@ package src.singleton;
  * Singleton
  */
 public class Singleton {
+    // If initialized here (which is thread-safe), the synchronized
+    // block is not necessary.
+    // ex: private static final Singleton uniqueIntence = new Singleton();
     private volatile static Singleton uniqueInstance;
 
     private Singleton() {
     }
 
     public static Singleton getInstance() {
+        // "double-checked locking" pattern. Works since 'uniqueIntance' is volatile:
         if (uniqueInstance == null) {
             synchronized (Singleton.class) {
                 if (uniqueInstance == null) {
